ccb8efc6e987d62ba5e2cffb323d50911beedf94
victorlin12345 <victorlin12345@gmail.com>
2018-10-30 18:20:58 +0800

    home css

--- services/css_home.rb
+++ services/css_home.rb
@@ -11,17 +11,17 @@ class CSSHome
     def call()
       @cates.map! do |c|
         if c["name"] == "食"
-          c = c.merge!("css_progs"=>"eat_progs","css_img"=>"eat_image")
+          c = c.merge!("css_progs"=>"eat_progs","css_img"=>"eat_image","img"=>"/home/eaticon.png","flag"=>"eat_flag")
         elsif c['name'] == '衣'
-          c = c.merge!("css_progs"=>"cloth_progs","css_img"=>"cloth_image")
+          c = c.merge!("css_progs"=>"cloth_progs","css_img"=>"cloth_image","img"=>"/home/clothicon.png","flag"=>"cloth_flag")
         elsif c['name'] == '住'
-          c = c.merge!("css_progs"=>"live_progs","css_img"=>"live_image")
+          c = c.merge!("css_progs"=>"live_progs","css_img"=>"live_image","img"=>"/home/liveicon.png","flag"=>"live_flag")
         elsif c['name'] == '行'
-          c = c.merge!("css_progs"=>"traffic_progs","css_img"=>"traffic_image")
+          c = c.merge!("css_progs"=>"traffic_progs","css_img"=>"traffic_image","img"=>"/home/trafficicon.png","flag"=>"traffic_flag")
         elsif c['name'] == '育'
-          c = c.merge!("css_progs"=>"education_progs","css_img"=>"education_image")
+          c = c.merge!("css_progs"=>"education_progs","css_img"=>"education_image","img"=>"/home/educationicon.png","flag"=>"education_flag")
         elsif c['name'] == '樂'
-          c = c.merge!("css_progs"=>"fun_progs","css_img"=>"fun_image")
+          c = c.merge!("css_progs"=>"fun_progs","css_img"=>"fun_image","img"=>"/home/funicon.png","flag"=>"fun_flag")
         end
       end
       @info['cates'] = @cates
