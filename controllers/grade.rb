2cd9de3bae44b2032643283da438bd836b5c09b6
victorlin12345 <victorlin12345@gmail.com>
2018-11-02 15:38:12 +0800

    good2

--- controllers/grade.rb
+++ controllers/grade.rb
@@ -23,17 +23,21 @@ module Howtosay
        
         # 存到 good_question, good_detail, good_sentence
         routing.post do
-          account_id = @current_account["id"]
-          cate_id = routing.params["cate_id"]
-          task_id = routing.params['task_id']
-          answer_size = routing.params['answer_size'].to_i
-          @answers = {}
-          answer_size.times do |i|
-            answer_id, related = routing.params["answer#{i+1}"].split(':')
-            @answers[answer_id] = related.to_i
+          unless @current_account.nil?
+            account_id = @current_account["id"]
+            cate_id = routing.params["cate_id"]
+            task_id = routing.params['task_id']
+            answer_size = routing.params['answer_size'].to_i
+            @answers = {}
+            answer_size.times do |i|
+              answer_id, related = routing.params["answer#{i+1}"].split(':')
+              @answers[answer_id] = related.to_i
+            end
+            SaveGrade.new(App.config).call(account_id, task_id, @answers)
+            routing.redirect "/grade/#{cate_id}"
+          else
+            routing.redirect '../../auth/login'
           end
-          SaveGrade.new(App.config).call(account_id, task_id, @answers)
-          routing.redirect "/grade/#{cate_id}"
         end
       end
     end
